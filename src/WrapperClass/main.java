db756d4ded306f6f7c7805682d598b9584872c8b
Cuong-galaxy <manhcuonghn94@gmail.com>
2025-08-17 13:52:55 +0900

    test git lân 2

--- src/WrapperClass/main.java
+++ src/WrapperClass/main.java
@@ -2,6 +2,8 @@ package WrapperClass;
 
 public class main {
     public static void main(String[] args) {
+        System.out.println("test git lần 2");
         System.out.println("Test học git");
     }
+
 }
